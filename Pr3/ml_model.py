cb87ae0a03baedbd68332682ba77a31f2746bd1a
wolfymmm <marusina.yana@lll.kpi.ua>
2025-04-24 11:57:00 +0300

    comment

--- Pr3/ml_model.py
+++ Pr3/ml_model.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 def train_model(df_model):
     X = df_model[['temperature_celsius', 'humidity', 'cloud', 'uv_index']]
     y = df_model['wind_kph']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=25)
 
     model = RandomForestRegressor(n_estimators=100, random_state=25)
     model.fit(X_train, y_train)
